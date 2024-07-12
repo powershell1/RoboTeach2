065c0b567708f6843ec25b55559da504fc951352
powershell1 <175_40628@st.bcc1852.com>
2024-07-12 19:25:57 +0700

    Updates

--- iframe/blocks/text.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-export declare const textBlocks: {
-    [key: string]: any;
-};
-//# sourceMappingURL=text.d.ts.map
