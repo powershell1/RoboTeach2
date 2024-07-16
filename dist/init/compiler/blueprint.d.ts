520a1ea40eaead744836b7bc7b5e5766208d8f5d
powershell1 <175_40628@st.bcc1852.com>
2024-07-17 01:37:31 +0700

    final

--- dist/init/compiler/blueprint.d.ts
+++ dist/init/compiler/blueprint.d.ts
@@ -3,8 +3,11 @@ import { BlockCode } from "./blockCode";
 declare function craftBlock(workspace: EmulatorWorkspaces, blockData: {
     [id: string]: any;
 }): BlockCode;
+declare function blockPicker(inputs: {
+    [id: string]: any;
+}): any;
 declare function compile(workspace: EmulatorWorkspaces, json: {
     [id: string]: any;
 }): Promise<void>;
-export { compile, craftBlock };
+export { compile, craftBlock, blockPicker };
 //# sourceMappingURL=blueprint.d.ts.map
